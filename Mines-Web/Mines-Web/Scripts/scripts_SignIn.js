c9f5bb917fcb4a141be7a80e7c8d4cd0bf1e5957
Brydon Johnson <brykeith@gmail.com>
2021-01-15 08:30:51 -0700

    added SignIn view & styles & scripts to master

---
+++ Mines-Web/Mines-Web/Scripts/scripts_SignIn.js
@@ -0,0 +1,9 @@
+﻿const btns_SwitchForms = document.querySelectorAll(".switch-form-button");
+const slide_container = document.querySelector(".slide-container");
+
+
+btns_SwitchForms.forEach((item) =>
+    item.addEventListener("click", () => {
+        slide_container.classList.toggle("slide");
+    })
+);
