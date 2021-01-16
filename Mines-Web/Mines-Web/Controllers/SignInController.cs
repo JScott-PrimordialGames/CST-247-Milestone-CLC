bc6e41ca51cd1bd7afb6fd9f846e52b3cfcdb1da
Brydon Johnson <brykeith@gmail.com>
2021-01-15 20:46:37 -0700

    split the signIn view in to partial views for sign in and register

---
+++ Mines-Web/Mines-Web/Controllers/SignInController.cs
@@ -0,0 +1,38 @@
+﻿using Mines_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mines_Web.Controllers
+{
+    public class SignInController : Controller
+    {
+        // GET: SignIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoadRegForm()
+        {
+            return PartialView("_RegisterForm");
+        }
+
+        public ActionResult LoadSignInForm()
+        {
+            return PartialView("_SignInForm");
+        }
+
+        public ActionResult Authenticate(PrincipalModel model)
+        {
+            return Content(model.UserName);
+        }
+
+        public ActionResult Register(UserModel model)
+        {
+            return Content(model.Username);
+        }
+    }
+}
