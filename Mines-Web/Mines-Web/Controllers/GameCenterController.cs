e1d3e15c34214606415ebe70953925667f3ed888
Brydon Johnson <brykeith@gmail.com>
2021-01-21 20:33:48 -0700

    added GameCenter Controller and GameCenter view.

---
+++ Mines-Web/Mines-Web/Controllers/GameCenterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mines_Web.Controllers
+{
+    public class GameCenterController : Controller
+    {
+        // GET: GameCenter
+        public ActionResult Index()
+        {
+            return View("GameCenter");
+        }
+
+        public ActionResult LoadGameBoard()
+        {
+            return PartialView("_GameBoard");
+        }
+    }
+}
