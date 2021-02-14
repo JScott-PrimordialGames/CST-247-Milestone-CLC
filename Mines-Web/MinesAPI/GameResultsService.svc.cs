01d66ccfacab5395d44dd29a86b0e1bc4cfeed35
Donald_Trowbridge <dtrowbridg@my.gcu.edu>
2021-02-14 12:45:06 -0700

    Added API call to get all results for a game difficulty.

--- Mines-Web/MinesAPI/GameResultsService.svc.cs
+++ Mines-Web/MinesAPI/GameResultsService.svc.cs
@@ -100,11 +100,12 @@ namespace MinesAPI
             int userId = 0;
             int difficultyLevel = 0;
             Int32.TryParse(id, out userId);
+            GameResultsDTO gameResultsDTO;
 
             if (!difficulty.ToLower().Equals("beginner") && !difficulty.ToLower().Equals("intermediate") && !difficulty.ToLower().Equals("expert")
                      && !difficulty.ToLower().Equals("1") && !difficulty.ToLower().Equals("2") && !difficulty.ToLower().Equals("3"))
             {
-                GameResultsDTO gameResultsDTO = new GameResultsDTO();
+                gameResultsDTO = new GameResultsDTO();
                 gameResultsDTO.ErrorCode = 400;
                 gameResultsDTO.ErrorMessage = "Bad Request: The difficulty level {0} does not exist sent is not valid. Please enter one of the following:" +
                     "\"beginner\", \"intermediate\", \"expert\", \"1\", \"2\", \"3\".";
@@ -126,7 +127,7 @@ namespace MinesAPI
 
             if (userId <= 0)
             {
-                GameResultsDTO gameResultsDTO = new GameResultsDTO();
+                gameResultsDTO = new GameResultsDTO();
                 gameResultsDTO.ErrorCode = 400;
                 gameResultsDTO.ErrorMessage = "Bad Request: The user id sent is not valid";
                 return gameResultsDTO;
@@ -136,7 +137,7 @@ namespace MinesAPI
                 GameResultsBusinessService service = new GameResultsBusinessService();
                 if (service.UserExists(userId))
                 {
-                    GameResultsDTO gameResultsDTO = new GameResultsDTO();
+                    gameResultsDTO = new GameResultsDTO();
                     gameResultsDTO.ErrorCode = 0;
                     gameResultsDTO.ErrorMessage = "OK";
                     gameResultsDTO.GameResults = service.GetGameResultsForUserAndGameDifficulty(userId, difficultyLevel);
@@ -144,13 +145,53 @@ namespace MinesAPI
                 }
                 else
                 {
-                    GameResultsDTO gameResultsDTO = new GameResultsDTO();
+                    gameResultsDTO = new GameResultsDTO();
                     gameResultsDTO.ErrorCode = 404;
                     gameResultsDTO.ErrorMessage = "Not Found: There are no resources for the user with id: " + userId.ToString() + ".";
                     return gameResultsDTO;
                 }
 
             }
+        }
+
+        public GameResultsDTO GetGameResultsForGameDifficulty(string limit, string difficulty)
+        {
+            int limitInt = 0;
+            Int32.TryParse(limit, out limitInt);
+            int difficultyLevel = 0;
+            GameResultsDTO gameResultsDTO;
+
+            if (!difficulty.ToLower().Equals("beginner") && !difficulty.ToLower().Equals("intermediate") && !difficulty.ToLower().Equals("expert")
+                && !difficulty.ToLower().Equals("1") && !difficulty.ToLower().Equals("2") && !difficulty.ToLower().Equals("3"))
+            {
+                gameResultsDTO = new GameResultsDTO();
+                gameResultsDTO.ErrorCode = 400;
+                gameResultsDTO.ErrorMessage = "Bad Request: The difficulty level {0} does not exist sent is not valid. Please enter one of the following:" +
+                    "\"beginner\", \"intermediate\", \"expert\", \"1\", \"2\", \"3\".";
+                return gameResultsDTO;
+            }
+
+            if (difficulty.ToLower().Equals("beginner") || difficulty.Equals("1"))
+            {
+                difficultyLevel = 1;
+            }
+            else if (difficulty.ToLower().Equals("intermediate") || difficulty.Equals("2"))
+            {
+                difficultyLevel = 2;
+            }
+            else if (difficulty.ToLower().Equals("expert") || difficulty.Equals("3"))
+            {
+                difficultyLevel = 3;
+            }
+
+
+
+            GameResultsBusinessService service = new GameResultsBusinessService();
+            gameResultsDTO = new GameResultsDTO();
+            gameResultsDTO.ErrorCode = 0;
+            gameResultsDTO.ErrorMessage = "OK";
+            gameResultsDTO.GameResults = service.GetGameResultsForDifficulty(limitInt, difficultyLevel);
+            return gameResultsDTO;
         }
     }
 }
