4db433983c149fae51c5b00885f27c30cc1921d1
Jscott108 <61676954+jscott108@users.noreply.github.com>
2021-02-14 16:59:48 -0700

    Added ScoreService and ScoreDAO

---
+++ Mines-Web/Mines-Web/Services/Business/ScoreService.cs
@@ -0,0 +1,20 @@
+﻿using Mines_Web.Models;
+using Mines_Web.Services.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mines_Web.Services.Business
+{
+    public class ScoreService
+    {
+        ScoreDAO service = new ScoreDAO();
+
+        public bool AddScore(UserModel user, float score)
+        {
+            int userID = user.ID;
+            return service.AddScore(userID, score);
+        }
+    }
+}
---
+++ Mines-Web/Mines-Web/Services/Data/ScoreDAO.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Mines_Web.Services.Data
+{
+    public class ScoreDAO
+    {
+        //Configuration string from web.config
+        string connectionStr = ConfigurationManager.ConnectionStrings["MinesApp"].ConnectionString;
+
+        //Stored Procedure List
+        string spAddScore = "[DBO].[SP_AddScore]";
+
+
+        public bool AddScore(int userId, float score)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(spAddScore, conn))
+                {
+                    SqlParameter isInserted = new SqlParameter("@InsertSucceeded", System.Data.SqlDbType.Bit) { Direction = System.Data.ParameterDirection.Output };
+
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@UserId", userId);
+                    cmd.Parameters.AddWithValue("@Score", score);
+                    cmd.Parameters.Add(isInserted);
+
+                    try
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        conn.Close();
+
+                        if (Convert.ToInt32(isInserted.Value) == 1)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        Debug.WriteLine("Exception: {0}: {1}\n{2}", ex.Number, ex.Message, ex.Errors);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine("Exception:" + ex.Message);
+                    }
+                    return false;
+                }
+            }
+        }     
+    }
+}
