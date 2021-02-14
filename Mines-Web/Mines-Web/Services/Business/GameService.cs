4e9bbe40a7b85fc082f24a32ff6b4183762a5c95
Brydon Johnson <brykeith@gmail.com>
2021-02-13 19:20:18 -0700

    implemented save game functionality

--- Mines-Web/Mines-Web/Services/Business/GameService.cs
+++ Mines-Web/Mines-Web/Services/Business/GameService.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Mines_Web.Models;
+using Newtonsoft.Json;
 
 namespace Mines_Web.Services.Business
 {
@@ -10,9 +12,24 @@ namespace Mines_Web.Services.Business
     {
         GamesDAO service = new GamesDAO();
 
-        public bool SaveGame(GameObject gameObject)
+        public bool SaveGame(UserModel user, BoardModel board)
         {
-            return service.SaveGame(gameObject);
+            int userID = user.ID;
+            string boardJsonString = JsonConvert.SerializeObject(board);
+            return service.SaveGame(userID, boardJsonString);
+        }
+
+        public BoardModel LoadGame(int gameID)
+        {
+            string boardJsonString = service.LoadGame(gameID);
+            BoardModel board = JsonConvert.DeserializeObject<BoardModel>(boardJsonString);
+            return board;
+            
+        }
+
+        public List<int> GetThreeSavedGamesByUserID(int userID)
+        {
+            return service.GetThreeSavedGamesByUserID(userID);
         }
 
     }
--- Mines-Web/Mines-Web/Services/Data/GamesDAO.cs
+++ Mines-Web/Mines-Web/Services/Data/GamesDAO.cs
@@ -17,10 +17,12 @@ namespace Mines_Web.Services.Data
 
         //Stored Procedure List
         string spSaveGame = "[DBO].[SP_SaveGame]";
+        string spLoadGame = "[DBO].[SP_LoadGame]";
+        string sp_GetThreeSavedGamesByUserID = "[DBO].[SP_GetThreeSavedGamesByUserID]";
 
 
 
-        public bool SaveGame(GameObject gameObject)
+        public bool SaveGame(int userID, string boardJsonString)
         {
             using (SqlConnection conn = new SqlConnection(connectionStr))
             {
@@ -30,7 +32,8 @@ namespace Mines_Web.Services.Data
                     SqlParameter isSuccessful = new SqlParameter("@AddSucceeded", System.Data.SqlDbType.Bit) { Direction = System.Data.ParameterDirection.Output };
 
                     cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@GameString", gameObject.JSONstring);
+                    cmd.Parameters.AddWithValue("@GameString", boardJsonString);
+                    cmd.Parameters.AddWithValue("@userId", userID);
                     cmd.Parameters.Add(isSuccessful);
 
                     try
@@ -62,9 +65,90 @@ namespace Mines_Web.Services.Data
             }
         }
 
-        public bool LoadGame()
+        public string LoadGame(int gameID)
         {
-            return false;
+            string boardJsonString = "";
+            using (SqlConnection conn = new SqlConnection(connectionStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(spLoadGame, conn))
+                {
+
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Id", gameID);
+
+
+                    try
+                    {
+                        conn.Open();
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        reader.Read();
+
+                        if (reader.HasRows)
+                        {
+                            boardJsonString = reader["GameString"].ToString();
+                        }
+
+                        reader.Close();
+                        conn.Close();
+
+                    }
+                    catch (SqlException ex)
+                    {
+                        Debug.WriteLine("Exception: {0}: {1}\n{2}", ex.Number, ex.Message, ex.Errors);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine("Exception: {0}" + ex.Message);
+                    }
+
+
+                }
+            }
+            return boardJsonString;
+        }
+
+        public List<int> GetThreeSavedGamesByUserID(int userID)
+        {
+            List<int> gamesList = new List<int>();
+
+            using (SqlConnection conn = new SqlConnection(connectionStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(sp_GetThreeSavedGamesByUserID, conn))
+                {
+
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@userId", userID);
+
+
+                    try
+                    {
+                        conn.Open();
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            gamesList.Add(reader.GetInt32(0));
+                        }
+
+                        reader.Close();
+                        conn.Close();
+
+                    }
+                    catch (SqlException ex)
+                    {
+                        Debug.WriteLine("Exception: {0}: {1}\n{2}", ex.Number, ex.Message, ex.Errors);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine("Exception: {0}" + ex.Message);
+                    }
+
+
+                }
+            }
+            return gamesList;
+
+
         }
     }
 }
